f296cefe8f6a8a763ea34eb89900602240a515ba
swpclass <fraher@Christophers-MacBook-Pro.local>
2013-10-31 01:42:42 -0500

    Added Delete

--- js/database_scripts.js
+++ js/database_scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function(e) {
     	html5sql.openDatabase("com.studybuddy.appdb", "Study Buddy Assignments", 3*1024*1024);
 		
 		// Index.html
-		if(window.location.pathname == 'index.html') 
+		if(window.location.href.indexOf("index.html") > -1) 
 		{
 			
 			
@@ -11,11 +11,12 @@ $(document).ready(function(e) {
 		["SELECT * FROM test;"],
 		function(transaction, results, rowsArray){
 			for(var items = 0; items < rowsArray.length; items++){
-				var id = rowsArray[items].ID;
+				var id = rowsArray[items].id;
+				var className = rowsArray[items].className;
 				var assignmentName = rowsArray[items].assignmentName;
 				var assignmentDescription = rowsArray[items].assignmentDescription;	
 				
-				$("#list").append("<li><a href='#demo-mail'><h3>CSE 3330</h3><p class='topic'><strong>" + assignmentName + "</strong></p><p>" + assignmentDescription + " </p><p class='ui-li-aside'><strong>4:48</strong>PM</br><strong>10/1/2013</strong></p></a><a href='#' class='delete'>Delete</a></li>").listview("refresh");
+				$("#list").append("<li ><a href='#demo-mail'><h3>" + className + "</h3><p class='topic'><strong>" + assignmentName + "</strong></p><p>" + assignmentDescription + " </p><p class='ui-li-aside'><strong>4:48</strong>PM</br><strong>10/1/2013</strong></p></a><a id='" + id + "' href='#' class='delete'>Delete</a></li>").listview("refresh");
 				
 				
 			}
@@ -24,5 +25,7 @@ $(document).ready(function(e) {
 			alert('Error retrieving assignments.');	
 		});
 		}
-
+		
     });
+	
+	
